0a36e5a1d82ff19f02540cc882774d3cb7d8dda9
Daniel Kolocka <kolockadaniel@hotmail.com>
2025-05-22 22:23:56 -0700

    add effect to change color of grid elemnt on mouse hover

--- javascript.js
+++ javascript.js
@@ -1,19 +1,28 @@
 const container = document.querySelector(".container");
 
 
-
 function createGrid(len) {
     const size = 100/len;
     for (let i=0; i<(len*len); i++) {
         const gridSquare = document.createElement("div");
         gridSquare.setAttribute("class", "gridItem");
         gridSquare.setAttribute("style", `width: ${size}%; height: ${size}%`);
-        gridSquare.textContent = "-";
+        // gridSquare.textContent = "";
             
         container.appendChild(gridSquare);
     }
 }
 
 createGrid(16);
+
+const gridItems = document.querySelectorAll(".gridItem");
+
+gridItems.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        let oldStyle = item.getAttribute("style");
+        item.setAttribute("style", oldStyle + "; background-color: red");
+        // item.style.backgroundColor = 'red';
+    });
+});
 
 
